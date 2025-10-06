c255f59b2bd07fce1f96438e2ba1741f5f4c81ba
= <=>
2025-10-06 19:56:46 +0200

    Commit n. 3 di Developer_1

--- fattoriale.py
+++ fattoriale.py
@@ -1,9 +1,11 @@
 import math
 
 def fattoriale(n):
-    if (n >= 0) and (n - math.trunc(n) == 0):
+    try:
+        assert (n >= 0) and (n - math.trunc(n) == 0)
         if n == 0 or n == 1:
             return 1
         else:
             return n * fattoriale(n - 1)
-
+    except AssertionError:
+        return -1
