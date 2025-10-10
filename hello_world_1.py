d4b4239f350f6c31f33c3802dc3be97e98c00d92
= <=>
2025-10-10 17:57:35 +0200

    Commit n. 3 di Developer_1

--- hello_world_1.py
+++ hello_world_1.py
@@ -1,23 +1,2 @@
-while True:
-    print("Ciao Mondo!")
-    print("bonjour au monde entier!")
-    if True:
-        print("addio")
-    else:
-        print("hallo")
-    break
-
-while True:
-    print("Ciao Mondo!")
-    print("bonjour au monde entier!"
-    print("   ")
-    break
-
-while True:
-    print("Ciao Mondo!")
-    print("bonjour au monde entier!")
-    if True:
-        print("addio")
-    else:
-        print("hallo")
-    break
+print("Ciao Mondo!")
+print("bonjour au monde entier!")
